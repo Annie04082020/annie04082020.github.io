b8717a9749da6ab3b59c17089a302c0036528cea
Annie Huang <annie.huangah@proton.me>
2026-07-29 17:16:13 +0900

    upload attachment: lseg_mon.v

---
+++ public/attachments/1785312972373-lseg_mon.v
@@ -0,0 +1,19 @@
+module lseg_mon(
+
+)
+    input beep;
+
+    // beep data input ?;
+    // timer data input ?;
+    // stopwatch data input ?;
+    
+    output o_beep;
+    output [3:0]o_a;
+    output [3:0]o_b;
+    output [3:0]o_c;
+    output [3:0]o_d;
+    output [3:0]o_e;
+    output [3:0]o_f;
+    output [3:0]o_g;
+
+endmodule
